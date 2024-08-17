eb154d19625587595f31ff8391b3d3751f71abd2
IoriFernando <iori.fcr@gmail.com>
2024-08-17 10:28:44 -0300

    Adicionando comentários

--- DHT11.ino
+++ DHT11.ino
@@ -1,29 +1,30 @@
-#include "DHT.h"
+#include "DHT.h" // Importação das biblilotecas
 
-#define DHTPIN 4
-#define DHTTYPE DHT11
+#define DHTPIN 4 // Definindo o pino da placa
+#define DHTTYPE DHT11 // Definito o tipo de sensor
 
-DHT dht(DHTPIN, DHTTYPE);
-
+DHT dht(DHTPIN, DHTTYPE); // Cria um objeto do Tipo DHT 
+// O objeto rebedo o pino e o tipo do sensor
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Taxa de transmissão
   Serial.println(F("DHT11 Test!"));
-  dht.begin();
+  dht.begin(); // inicialização do sensor 
 }
 
 void loop() {
-  delay(2000);
+  delay(2000); // Taxa de leitura de 2s 
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float h = dht.readHumidity(); // Variável para umidade
+  float t = dht.readTemperature(); // Variável para a temperatura
 
-  if(isnan(h)|| isnan(t)){
+  if(isnan(h)|| isnan(t)){ // Teste se a leitura ocorre normalmente
     Serial.println(F("Falha na leitura do sensor DHT11"));
   }
 
-  float indiceCalor = dht.computeHeatIndex(t,h,false);
+  float indiceCalor = dht.computeHeatIndex(t,h,false); // Variável para o indice de calor.
 
+  // Visualização dos dados coletados
   Serial.print(F("Umidade: "));
   Serial.print(h);
   Serial.print(F("%  Temperatura:  "));
